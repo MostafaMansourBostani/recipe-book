001ce6db1a05b8b90afb1b4f769a14137b40c7ed
Mostafa Mansour Bostani <dragon.mg@gmail.com>
2017-06-01 18:19:19 +0430

    load local images

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,14 +12,14 @@ export class RecipeService {
     new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      '/assets/images/591be16625ff2.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      '/assets/images/591be340bae36.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
